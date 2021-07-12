e2689214d58be0f35a87638b4f25cf5b3a56b104
LMGXENON <82078620+LMGXENON@users.noreply.github.com>
2021-07-12 16:06:52 +0400

    Add files via upload

---
+++ config.js
@@ -0,0 +1,56 @@
+module.exports = {
+    bot: {
+        token: "ODU0NzczNzIzODMwNjgxNjgw.YMo0EA.y_LuokDwBG7nN2k8E99mxBAQMyA",
+        prefix: "-",
+        owners: ["821696185647497256"],
+        mongourl: "mongodb+srv://xenon:waleedaamir2005@cluster0.xwsd4.mongodb.net/lmgxenon?retryWrites=true&w=majority"
+
+    },
+
+    website: {
+        callback: "https://discordbotlist.lmgxenon.repl.co/callback",
+        secret: "jQIq-VZDQNQb3huSFU62P_DqpmXZ4Faw",
+        clientID: "854773723830681680",
+        tags: [ "Moderation", "Fun", "Minecraft","Economy","Guard","NSFW","Anime","Invite","Music","Logging", "Web Dashboard", "Reddit", "Youtube", "Twitch", "Crypto", "Leveling", "Game", "Roleplay", "Utility", "Turkish" ]    
+    },
+
+    server: {
+        id: "844138740435124225",
+        roles: {
+            yonetici: "",
+            moderator: "852903256434278422",
+            profile: {
+                booster: "",
+                sponsor: "",
+                supporter: "",
+                partnerRole: ""
+            },
+            codeshare: {
+                javascript: "JS",
+                html: "HTML",
+                altyapilar: "ALTYAPILAR",
+                bdfd: "BDFD",
+                besdavet: "BESDAVET",
+                ondavet: "ONDAVET",
+                onbesdavet: "ONBESDAVET",
+                yirmidavet: "YIRMIDAVET"
+            },
+            botlist: {
+                developer: "821696185647497256",
+                certified_developer: "821696185647497256",
+                bot: "",
+                certified_bot: "858402618904936459"
+            }
+        },
+        channels: {
+            codelog: "864075080090648596",
+            login: "864075120683384846",
+            webstatus: "864075167247368192",
+            uptimelog: "864075206187810846",
+            botlog: "864075249547739136",
+            votes: "864075311599714314"
+        }
+    }
+
+
+}
